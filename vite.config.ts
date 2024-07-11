73e8fd77afccfa63a2d24d3f7bb889d43012c100
Xia Zhenchao <xiazhenchao96@gmail.com>
2024-07-11 11:46:01 -0400

    docker test6

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,8 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(),nodePolyfills()],
-  preview: {
-   port: 8080,
-   strictPort: true,
-  },
   server: {
-   port: 8080,
-   strictPort: true,
-   host: true,
-   origin: "http://0.0.0.0:8080",
+    port: 8080
   },
-});
+})
